6fca1c525b05e88975b4308558b4c195342bb326
Peter Dunne <peter.dunne@applied-magnetism.com>
2021-11-08 03:51:06 +0100

    build: cache fix for web deployment

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'caliphui';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './caliphui.js',
+  './caliphui_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
